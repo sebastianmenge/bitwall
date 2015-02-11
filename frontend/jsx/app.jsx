747a1f8676e8427b79666ce7bd1bbb8f34224909
sebastianmenge <an.sebastian.menge@gmail.com>
2015-02-11 15:06:53 +0100

    changes to appjsx

--- frontend/jsx/app.jsx
+++ frontend/jsx/app.jsx
@@ -6,19 +6,21 @@ var React = require("react"),
     Link = Router.Link,
     DefaultRoute = Router.DefaultRoute,
     RouteHandler = Router.RouteHandler,
-    Promise = require("bluebird");
+
+    Promise = require("bluebird"),
+    WallStore = require("./stores/flux/wall_store.js");
+    StoreState = require("./mixins/store_state.js");
 
 var Show = require("./sections/walls/_show.jsx");
 
 
 var App = React.createClass({
-  // mixins: [StoreState(Store.Educator)],
-  // getStoreState: function(){
-  //   return {
-  //     educator: Store.Educator.getEducatorAny(),
-  //     me: Store.Educator.getMeAny()
-  //   }
-  // },
+  mixins: [StoreState(WallStore)],
+  getStoreState: function(){
+    return {
+      walls: WallStore.getWalls(),
+    }
+  },
   componentDidMount: function() {
     console.log("triggered");
   },
@@ -37,18 +39,10 @@ var routes = (
   </Route>
 );
 
-window.onload = function() {
-  // Router.run(routes, function (Handler) {
-  //   React.render(<Index/>, document.getElementById("app"));
-  // });
+WallStore.init().then(function(){
   Router.run(routes, Router.HistoryLocation, function (Handler) {
     React.render(<Handler/>, document.getElementById("app"));
   });
-};
-
-
-// Store.Educator.init(EDUCATOR_ID).then(e => Store.Plan.init()).then(Store.Alert.init()).then(Store.Processing.init()).then(function(){
-//   React.renderComponent(routes, document.getElementById("app"))
-// })
+})
 
 window.React = React;
